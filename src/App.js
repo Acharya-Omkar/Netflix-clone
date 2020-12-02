127ab061e6e2a39e93a7af3bf70d95c236dfc6f4
Omkar Acharya <49238582+Acharya-Omkar@users.noreply.github.com>
2020-12-02 16:06:30 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+import request from './request';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Helloo</h1>"
+      <Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={request.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={request.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries}/>
+
+    </div>
+  );
+}
+
+export default App;
