cc691849d79f8adbbcfee0c5d8b81e53c3cd0a9b
Omkar Acharya <49238582+Acharya-Omkar@users.noreply.github.com>
2020-12-03 22:50:00 +0530

    Working on Nav bar implementation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Row from './Row';
 import request from './request';
 import Banner from './Banner';
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
+      <Nav />
      <Banner />
       <Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={request.fetchNetflixOriginals}/>
       <Row title="Trending Now" fetchUrl={request.fetchTrending}/>
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Nav.css";
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix logo"></img>
+            <img className="nav__avatar" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fpro2-bar-s3-cdn-cf1.myportfolio.com%2Fdddb0c1b4ab622854dd81280840458d3%2F98032aebff601c1d993e12a0_rw_600.png%3Fh%3D8030f4d5734548795c22da59ca72e3e1&imgrefurl=https%3A%2F%2Fheidiwalti.myportfolio.com%2Fnetflix-profile-avatars&tbnid=S5Pd47sSXFlwOM&vet=12ahUKEwiM2_b6oLHtAhWNCCsKHYnwDfgQMygQegUIARDCAQ..i&docid=FSsr8EOXFiNqbM&w=320&h=320&itg=1&q=avatar%20netflix%20profile%20icon&hl=en&ved=2ahUKEwiM2_b6oLHtAhWNCCsKHYnwDfgQMygQegUIARDCAQ.png" alt="Netflix logo" />
+        </div>
+    )
+}
+
+export default Nav
